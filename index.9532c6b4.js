c33214092151868d5a2b785633ed4eb23a70cac5
Vladyslav Kutsyn <vladislavkucn@gmail.com>
2025-02-04 18:01:36 +0100

    make build

---
+++ index.9532c6b4.js
@@ -0,0 +1,2 @@
+var r=document.querySelector("table");!function(e,l){var t=r.rows,o=!0,n=!1,a=void 0;try{for(var c,i=t[Symbol.iterator]();!(o=(c=i.next()).done);o=!0){var f=c.value,u=f.cells[1];if(u){var v=u.cloneNode(!0);f.insertBefore(v,f.cells[4])}}}catch(r){n=!0,a=r}finally{try{o||null==i.return||i.return()}finally{if(n)throw a}}}(0,0);
+//# sourceMappingURL=index.9532c6b4.js.map
